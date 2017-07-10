6e213219283fc451f428936422c06baa3f1a89d9
Liess <ejmaskarino05@gmail.com>
2017-07-11 06:36:17 +0800

    Homepage :+1:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,398 @@
-<!doctype html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+		
+		<!-- HEADER -->
+<!DOCTYPE html>
+<html lang="en">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+<link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" />
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <title>Explore CARAGA</title>
 
-            .position-ref {
-                position: relative;
-            }
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/fontawesome/css/font-awesome.min.css" type="text/css" rel="stylesheet">
+    <link href="vendor/animateit/animate.min.css" rel="stylesheet">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <!-- Vendor css -->
+    <link href="vendor/owlcarousel/owl.carousel.css" rel="stylesheet">
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
 
-            .content {
-                text-align: center;
-            }
+    <!--base and elements-->
+    <link href="css/custom1.css" rel="stylesheet">
+    <link href="css/custom2.css" rel="stylesheet">
 
-            .title {
-                font-size: 84px;
-            }
+    <!-- Responsive classes -->
+    <link href="css/responsive.css" rel="stylesheet">
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+    <!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+    <!-- Template color -->
+    <link href="css/color-variations/blue-dark.css" rel="stylesheet" type="text/css" media="screen" title="blue">
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    <!-- LOAD GOOGLE FONTS -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,800,700,600%7CRaleway:100,300,600,700,800" rel="stylesheet" type="text/css" />
+
+
+    <!--VENDOR SCRIPT-->
+    <script src="vendor/jquery/jquery-1.11.2.min.js"></script>
+    <script src="vendor/plugins-compressed.js"></script>
+    
+    
+    <!-- SLIDER REVOLUTION 5.x CSS SETTINGS -->
+    <link rel="stylesheet" type="text/css" href="vendor/rs-plugin/css/settings.css">
+    <link rel="stylesheet" type="text/css" href="vendor/rs-plugin/css/layers.css">
+    <link rel="stylesheet" type="text/css" href="vendor/rs-plugin/css/navigation.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/rs-plugin-styles.css">
+    
+    <!-- SLIDER REVOLUTION 5.x SCRIPTS  -->
+    <script type="text/javascript" src="vendor/rs-plugin/js/extensions/revolution.extension.video.min.js"></script>
+<script type="text/javascript" src="vendor/rs-plugin/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script type="text/javascript" src="vendor/rs-plugin/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script type="text/javascript" src="vendor/rs-plugin/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script type="text/javascript" src="vendor/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="vendor/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<script type="text/javascript" src="vendor/rs-plugin/js/extensions/revolution.extension.video.min.js"></script>
+<script type="text/javascript" src="vendor/rs-plugin/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script type="text/javascript" src="vendor/rs-plugin/js/extensions/revolution.extension.actions.min.js"></script>
+<script type="text/javascript" src="vendor/rs-plugin/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script type="text/javascript" src="vendor/rs-plugin/js/extensions/revolution.extension.navigation.min.js"></script>
+<script type="text/javascript" src="vendor/rs-plugin/js/extensions/revolution.extension.parallax.min.js"></script>
+</head>
+
+<body class="wide">
+    
+
+    <!-- WRAPPER -->
+    <div class="wrapper">
+
+		<!-- HEADER -->
+		<header id="header" class="header-logo-center">
+			<div id="header-wrap">
+				<div class="container">
+
+					<!--LOGO-->
+					<div id="logo">
+						<a href="home.html" class="logo" data-dark-logo="images/caragalogo.png">
+							<img src="images/caragalogo.png" alt="Polo Logo">
+						</a>
+					</div>
+					<!--END: LOGO-->
+
+					<!--MOBILE MENU -->
+					<div class="nav-main-menu-responsive">
+						<button class="lines-button x">
+							<span class="lines"></span>
+						</button>
+					</div>
+					<!--END: MOBILE MENU -->
+
+
+					<!--NAVIGATION-->
+					<div class="navbar-collapse collapse main-menu-collapse navigation-wrap">
+						<div class="container">
+							<nav id="mainMenu" class="main-menu mega-menu">
+								<ul class="main-menu nav nav-pills">
+									<li><a href="index.html"><i class="fa fa-home"></i></a>
+									</li>
+									<li><a href="">About</a></li>
+									<li><a href="">Explore</a></li>
+									<li class="dropdown"> <a href="#">Provinces </a>
+									</li>
+											<li class="dropdown"> <a href="#">Officials </a>
+				
+									</li>
+									
+									
+									
+													</div>
+												</div>
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+					<!--END: NAVIGATION-->
+				</div>
+			</div>
+		</header>
+		<!-- END: HEADER -->
+
+        <!-- REVOLUTION SLIDER -->
+        <div id="slider"> <div id="rev_slider_88_1_wrapper" class="rev_slider_wrapper fullwidthbanner-container" data-alias="youtube-hero" style="margin:0px auto;background-color:transparent;padding:0px;margin-top:0px;margin-bottom:0px;">
+				<!-- START REVOLUTION SLIDER 5.0.7 fullwidth mode -->
+					<div id="rev_slider_88_1" class="rev_slider fullwidthabanner" style="display:none;" data-version="5.0.7">
+						<ul>	<!-- SLIDE  -->
+							<li data-index="rs-270" data-transition="zoomout" data-slotamount="default"  data-easein="Power3.easeInOut" data-easeout="Power3.easeInOut" data-masterspeed="3000"  data-thumb="vendor/rs-plugin/examples/assets/images/youtubebg-100x50.jpg"  data-rotate="0"  data-saveperformance="off"  data-title="Intro" data-description="">
+								<!-- MAIN IMAGE -->
+								<img src="vendor/rs-plugin/examples/assets/images/caragabanner.jpg"  alt=""  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg" data-no-retina>
+								<!-- LAYERS -->
+
+								<!-- BACKGROUND VIDEO LAYER -->
+									<div class="rs-background-video-layer" 
+									data-forcerewind="on" 
+									data-volume="mute" 
+									data-videowidth="100%" 
+									data-videoheight="100%" 
+										data-videomp4="vendor/rs-plugin/examples/assets/videos/caraga.mp4"
+									data-videopreload="preload" 
+									data-videoloop="none" 
+									data-forceCover="1" 
+									data-aspectratio="16:9" 
+									data-autoplay="true" 
+									data-autoplayonlyfirsttime="false" 
+									data-nextslideatend="true" 
+										data-videoloop="loop" 
+								></div>
+								
+								<!-- LAYER NR. 1 -->
+								<div class="tp-caption tp-shape tp-shapewrapper   rs-parallaxlevel-0" 
+									 id="slide-270-layer-10" 
+									 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+									 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']" 
+												data-width="full"
+									data-height="full"
+									data-whitespace="nowrap"
+									data-transform_idle="o:1;"
+						 
+									 data-transform_in="opacity:0;s:1500;e:Power3.easeInOut;" 
+									 data-transform_out="s:300;s:300;" 
+									data-start="750" 
+									data-basealign="slide" 
+									data-responsive_offset="on" 
+									data-responsive="off"
+									
+									style="z-index: 5;background-color:rgba(0, 0, 0, 0.25);border-color:rgba(0, 0, 0, 0.50);"> 
+								</div>
+
+								<!-- LAYER NR. 2 -->
+								<div class="tp-caption NotGeneric-Icon   tp-resizeme rs-parallaxlevel-0" 
+									 id="slide-270-layer-8" 
+									 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+									 data-y="['middle','middle','middle','middle']" data-voffset="['-66','-64','-78','-78']" 
+												data-width="none"
+									data-height="none"
+									data-whitespace="nowrap"
+									data-transform_idle="o:1;"
+										data-style_hover="cursor:default;"
+						 
+									 data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:1500;e:Power4.easeInOut;" 
+									 data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" 
+									 data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;" 
+									 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" 
+									data-start="1000" 
+									data-splitin="none" 
+									data-splitout="none" 
+									data-responsive_offset="on" 
+
+									
+									style="z-index: 6; white-space: nowrap;"><i class="pe-7s-film"></i> 
+								</div>
+
+								<!-- LAYER NR. 3 -->
+								<div class="tp-caption NotGeneric-Title   tp-resizeme rs-parallaxlevel-0" 
+									 id="slide-270-layer-1" 
+									 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+									 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','-22','-29']" 
+												data-fontsize="['70','70','70','50']"
+									data-lineheight="['70','70','70','50']"
+									data-width="none"
+									data-height="none"
+									data-whitespace="nowrap"
+									data-transform_idle="o:1;"
+						 
+									 data-transform_in="z:0;rX:0deg;rY:0;rZ:0;sX:1.5;sY:1.5;skX:0;skY:0;opacity:0;s:1500;e:Power3.easeInOut;" 
+									 data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" 
+									 data-mask_in="x:0px;y:0px;" 
+									 data-mask_out="x:inherit;y:inherit;" 
+									data-start="1000" 
+									data-splitin="none" 
+									data-splitout="none" 
+									data-responsive_offset="on" 
+
+									
+									style="z-index: 7; white-space: nowrap;">A place full of wonders
+								</div>
+
+								<!-- LAYER NR. 4 -->
+								<div class="tp-caption NotGeneric-SubTitle   tp-resizeme rs-parallaxlevel-0" 
+									 id="slide-270-layer-4" 
+									 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+									 data-y="['middle','middle','middle','middle']" data-voffset="['52','52','28','13']" 
+												data-width="none"
+									data-height="none"
+									data-whitespace="nowrap"
+									data-transform_idle="o:1;"
+						 
+									 data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:1500;e:Power4.easeInOut;" 
+									 data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" 
+									 data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;" 
+									 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" 
+									data-start="1000" 
+									data-splitin="none" 
+									data-splitout="none" 
+									data-responsive_offset="on" 
+
+									
+									style="z-index: 8; white-space: nowrap;">Come and see the beauty of nature and our culture of CARAGA
+								</div>
+
+								<!-- LAYER NR. 5 -->
+								<div class="tp-caption Hero-Button rev-btn  rs-parallaxlevel-0" 
+									 id="slide-270-layer-7" 
+									 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+									 data-y="['middle','middle','middle','middle']" data-voffset="['124','124','80','65']" 
+												data-width="none"
+									data-height="none"
+									data-whitespace="nowrap"
+									data-transform_idle="o:1;"
+										data-transform_hover="o:1;rX:0;rY:0;rZ:0;z:0;s:300;e:Power1.easeInOut;"
+										data-style_hover="c:rgba(0, 0, 0, 1.00);bg:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 1.00);cursor:pointer;"
+						 
+									 data-transform_in="y:50px;opacity:0;s:1500;e:Power4.easeInOut;" 
+									 data-transform_out="y:[175%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" 
+									 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" 
+									data-start="1250" 
+									data-splitin="none" 
+									data-splitout="none" 
+									data-actions='[{"event":"click","action":"scrollbelow","offset":"0px"}]'
+									data-responsive_offset="on" 
+									data-responsive="off"
+									
+									style="z-index: 9; white-space: nowrap;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;">Explore Caraga 
+								</div>
+							</li>
+						</ul>
+						<div class="tp-static-layers">
+						</div>
+						<div class="tp-bannertimer tp-bottom" style="visibility: hidden !important;"></div>	
+					</div>
+				</div><!-- END REVOLUTION SLIDER -->
+					<!-- PARALLAX -->
+		<section class="parallax p-t-200 p-b-200 background-overlay-dark" style="background-image:url(images/caragapic1.jpg);" data-stellar-background-ratio="0.5">
+			<div class="container">
+				<div class="text-light">
+					<h1 class="text-medium m-b-5">Enjoy the nature</h1>
+					<h3>CARAGA is one of the regions that have so many </br> breathtaking beaches, mountains, springs, and lot of  </br>tourist spot that you'll like.</h3>
+				</div>
+			</div>
+		</section>
+			<section class="parallax p-t-200 p-b-200 background-overlay-dark" style="background-image:url(images/caragapic2.jpg);" data-stellar-background-ratio="0.5">
+			<div class="container">
+				<div class="text-light right">
+					<h1 class="text-medium m-b-5">Enjoy the culture</h1>
+					<h3>CARAGA is rich with cultural groups. See the fun and </br> colorful way of life with us.</h3>
+				</div>
+			</div>
+		</section>
+			<section class="parallax p-t-200 p-b-200 background-overlay-dark" style="background-image:url(images/caragapic3.jpg);" data-stellar-background-ratio="0.5">
+			<div class="container">
+				<div class="text-light">
+					<h1 class="text-medium m-b-5">Enjoy the foods</h1>
+					<h3>CARAGA have filipino dishes on it's own  style</br> and delicacies to boast to.</h3>
+				</div>
+			</div>
+		</section>
+		<!-- END: PARALLAX -->
+				<script type="text/javascript">
+					
+					var tpj=jQuery,			
+						revapi88;
+
+					tpj(document).ready(function() {
+						if(tpj("#rev_slider_88_1").revolution == undefined){
+							revslider_showDoubleJqueryError("#rev_slider_88_1");
+						}else{
+							revapi88 = tpj("#rev_slider_88_1").show().revolution({
+								sliderType:"standard",
+								jsFileLocation: "vendor/rs-plugin/js/",
+								sliderLayout:"fullwidth",
+								dottedOverlay:"none",
+								delay:9000,
+								navigation: {
+									onHoverStop:"off",
+								},
+								responsiveLevels:[1240,1024,778,480],
+								gridwidth:[1240,1024,778,480],
+								gridheight:[700,500,400,270],
+								lazyType:"none",
+								parallax: {
+									type:"scroll",
+									origo:"slidercenter",
+									speed:2000,
+									levels:[2,3,4,5,6,7,12,16,10,50],
+								},
+								shadow:0,
+								spinner:"off",
+								stopLoop:"on",
+								stopAfterLoops:0,
+								stopAtSlide:1,
+								shuffle:"off",
+								autoHeight:"off",
+								disableProgressBar:"on",
+								hideThumbsOnMobile:"off",
+								hideSliderAtLimit:0,
+								hideCaptionAtLimit:0,
+								hideAllCaptionAtLilmit:0,
+								debugMode:false,
+								fallbacks: {
+									simplifyAll:"off",
+									nextSlideOnWindowFocus:"off",
+									disableFocusListener:false,
+								}
+							});
+						}
+					});	/*ready*/
+				</script></div>
+        <!-- END REVOLUTION SLIDER -->
+
+        <!-- FOOTER -->
+        <footer class="background-dark text-grey" id="footer">
+            <div class="footer-content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12\">
+                            <div class="footer-logo float-left">
+                                <img alt="" class="logo" src="images/caragalogo.png">
+                            </div>
+                            <p style="margin-top: 12px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Suspendisse consectetur fringilla luctus. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.
+                            </p>
+
+                        </div>
+                    </div>
+	
             </div>
-        </div>
-    </body>
-</html>
+	
+        </footer>
+        <!-- END: FOOTER -->
+
+    </div>
+    <!-- END: WRAPPER -->
+
+    <!-- GO TOP BUTTON -->
+    <a class="gototop gototop-button" href="#"><i class="fa fa-chevron-up"></i></a>
+
+    <!-- Theme Base, Components and Settings -->
+    <script src="js/theme-functions.js"></script>
+
+    <!-- Custom js file -->
+    <script src="js/custom.js"></script>
+
+
+
+</body>
+
+</html>	
+		
