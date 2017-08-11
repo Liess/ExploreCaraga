117cd16e48fb02eca4669370d9745ac46eecad20
Liess <ejmaskarino05@gmail.com>
2017-08-12 03:59:42 +0800

    partial navigation

--- resources/views/admin/maintenance/mastermainte.blade.php
+++ resources/views/admin/maintenance/mastermainte.blade.php
@@ -3,7 +3,7 @@
     <head>
 
         <!-- Title -->
-        <title>Dashboard</title>
+        <title>Maintenance - @yield('Maintenance title')</title>
 
         <meta content="width=device-width, initial-scale=1" name="viewport"/>
         <meta charset="UTF-8">
@@ -133,7 +133,38 @@
                     </div>
                 </div>
             </div><!-- Navbar -->
-        @include('includes.Navigation')
+            <div class="page-sidebar sidebar">
+                <div class="page-sidebar-inner slimscroll">
+                    <div class="sidebar-header">
+                        <div class="sidebar-profile">
+                            <a href="javascript:void(0);" id="profile-menu-link">
+                                <div class="sidebar-profile-image">
+                                    <img src="images/venmar.jpg" class="img-circle img-responsive" alt="">
+                                </div>
+                                <div class="sidebar-profile-details">
+                                    <span>Venmar Vicente<br><small>Admin</small></span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <ul class="menu accordion-menu">
+                        <li class="active"><a href="/Dashboard" class="waves-effect waves-button"><span class="menu-icon glyphicon glyphicon-home"></span><p>Dashboard</p></a></li>
+                        <li><a href="#" class="waves-effect waves-button"><span class="menu-icon glyphicon glyphicon-user"></span><p>Profile</p></a></li>
+                        <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon fa fa-wrench"></span><p>Maintenance</p><span class="arrow"></span></a>
+                            <ul class="sub-menu">
+                                <li><a href="#">province</a></li>
+                            </ul>
+                        </li>
+                        <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon fa fa-book"></span><p>Pages</p><span class="arrow"></span></a>
+                        </li>
+                        <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon fa fa-binoculars"></span><p>Tourist spots</p><span class="arrow"></span></a>
+                        </li>
+                        <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon icon-map"></span><p>Maps</p><span class="arrow"></span></a>
+                        </li>
+
+                    </ul>
+                </div><!-- Page Sidebar Inner -->
+            </div><!-- Page Sidebar -->
             <div class="page-inner">
                 <div class="page-title">
                     <h3>Dashboard</h3>
---
+++ resources/views/includes/Navigation.blade.php
@@ -0,0 +1,39 @@
+<!-- fixed navigation -->
+
+    <div class="page-sidebar sidebar">
+                <div class="page-sidebar-inner slimscroll">
+                    <div class="sidebar-header">
+                        <div class="sidebar-profile">
+                            <a href="javascript:void(0);" id="profile-menu-link">
+                                <div class="sidebar-profile-image">
+                                    <img src="images/venmar.jpg" class="img-circle img-responsive" alt="">
+                                </div>
+                                <div class="sidebar-profile-details">
+                                    <span>Venmar Vicente<br><small>Admin</small></span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <ul class="menu accordion-menu">
+                        <li {{ Request::is('/url') ? ' class="active"' : null }}><a href="/Dashboard" class="waves-effect waves-button"><span class="menu-icon glyphicon glyphicon-home"></span><p>Dashboard</p></a></li>
+                        <li><a href="#" class="waves-effect waves-button"><span class="menu-icon glyphicon glyphicon-user"></span><p>Profile</p></a></li>
+                        <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon fa fa-wrench"></span><p>Maintenance</p><span class="arrow"></span></a>
+                            <ul class="sub-menu">
+                                <li><a href="#">Provinces</a></li>
+                                <li><a href="#">Cities</a></li>
+                                <li><a href="#">Tags</a></li>
+                                <li><a href="#">Destinations</a></li>
+                            </ul>
+                        </li>
+                        <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon fa fa-book"></span><p>Pages</p><span class="arrow"></span></a>
+                        </li>
+                        <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon fa fa-binoculars"></span><p>Tourist spots</p><span class="arrow"></span></a>
+                        </li>
+                        <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon icon-map"></span><p>Maps</p><span class="arrow"></span></a>
+                        </li>
+
+                    </ul>
+                </div><!-- Page Sidebar Inner -->
+            </div><!-- Page Sidebar -->
+
+<!-- /fixed navigation -->
