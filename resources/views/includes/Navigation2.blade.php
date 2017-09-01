c1f890e13a6ae499144089a572bf1004bd476a80
Liess <ejmaskarino05@gmail.com>
2017-09-01 21:41:30 +0800

    [new] website - navigation

---
+++ resources/views/includes/Navigation2.blade.php
@@ -0,0 +1,25 @@
+  					<div class="navbar-collapse collapse main-menu-collapse navigation-wrap">
+  						<div class="container">
+  							<nav id="mainMenu" class="main-menu mega-menu">
+  								<ul class="main-menu nav nav-pills">
+  									<li><a href="/">Home</a>
+  									</li>
+  									<li><a href="?About">About</a></li>
+  									<li class="active"> <a href="/Provinces">Provinces </a>
+                      			<li><a href="/Map">Explore</a></li>
+  									</li>
+
+
+  									</li>
+
+
+
+  													</div>
+  												</div>
+  											</li>
+  										</ul>
+  									</li>
+  								</ul>
+  							</nav>
+  						</div>
+  					</div>
